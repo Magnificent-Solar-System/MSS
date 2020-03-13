a24d27cecb5fed296ea31d155f1d838697269732
Tulen <56605335+KislyjKisel@users.noreply.github.com>
2020-03-13 10:12:33 +0300

    Remove compute context
    For mobile support

--- loading/initialization.js
+++ loading/initialization.js
@@ -5,7 +5,7 @@ canvas.height = document.body.clientHeight; //document.height is obsolete
 canvasW = canvas.width;
 canvasH = canvas.height;
 
-gl = canvas.getContext("webgl2-compute");  
+gl = canvas.getContext("webgl2");  
 gl.frontFace(gl.CW);
 gl.enable(gl.DEPTH_TEST);
 gl.depthFunc(gl.GEQUAL);
