75dd9da280d6ddef1873d2475548d687a81bf9d4
Tulen <56605335+KislyjKisel@users.noreply.github.com>
2020-03-15 13:05:57 +0300

    addpp

---
+++ technique/postprocessing/TechniquePostprocessing.js
@@ -0,0 +1,18 @@
+class TechniquePostProcessing extends Technique {
+    constructor() {
+        super();
+        super.LoadShaders("/technique/postprocessing/TechniquePostprocessing");
+        super.Link();
+        this.locationSampler = super.GetUniformLocation("uSampler");
+        
+        super.InitializeAttributes(
+            ["vPosition", 2, gl.FLOAT ]
+        );
+    }
+    Use(texture) {
+        super.Use();
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.uniform1i(this.locationSampler, 0);
+    }
+}
