893f9846cc729a62b1f138d26ed6bf61c104cc00
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2020-03-16 10:26:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function mainLoop(time_now) {
     t += deltaTime;
     
     camera.update();
+    camera.yaw = t;
     let viewProjectionMatrix = new mat4();
     mat4.Multiply(viewProjectionMatrix, camera.viewMatrix, projectionMatrix);
     
